c0d0fe4d05e6585ced80f3a6b155dae9e7083008
andre campos <andre321.ac@gmail.com>
2024-04-26 15:51:14 -0300

    variaveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,12 @@
+public class TiposVariaveis{
+
+    public static void main(String[] args) {
+        final double VALOR_DE_PI = 3.14;
+        float valor = 321.F;
+        System.out.println(VALOR_DE_PI);
+        System.out.println(valor);
+    }
+
+
+
+}
